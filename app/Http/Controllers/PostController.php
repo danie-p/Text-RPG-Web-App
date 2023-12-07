85ffd159be8f1a52537c5069f36f79ab5c22fcd2
Daniela Pavlíková <danielka.pavlikova@gmail.com>
2023-12-07 07:59:25 +0100

    updated delete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,4 +39,14 @@ class PostController extends Controller
         $post->update($incomingFields);
         return redirect('/roleplay');
     }
+
+    public function deletePost(Post $post) {
+        if (auth()->user()->id !== $post['user_id']) {
+            return redirect('/');
+        }
+
+        $post->delete();
+
+        return redirect('/roleplay');
+    }
 }
